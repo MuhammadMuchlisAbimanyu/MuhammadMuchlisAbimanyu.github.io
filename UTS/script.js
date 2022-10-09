0c2a3cc1b4bdd876dccbade4dce490dff3749fbe
MuhammadMuchlisAbimanyu <92872429+MuhammadMuchlisAbimanyu@users.noreply.github.com>
2022-10-09 18:03:41 +0800

    Add files via upload

---
+++ UTS/script.js
@@ -0,0 +1,45 @@
+var icon = document.getElementById('icon');
+
+if(localStorage.getItem('theme') == null){
+    localStorage.setItem('theme','light'); 
+}
+
+let localData = localStorage.getItem('theme');
+
+if(localData == 'light'){
+    icon.src = 'image/icon/moon.png';
+    document.body.classList.remove('dark-theme');
+}else if(localData == 'dark'){
+    icon.src = 'image/icon/sun.png';
+    document.body.classList.add('dark-theme');
+}
+
+icon.onclick = function(){
+    document.body.classList.toggle('dark-theme');
+    if(document.body.classList.contains('dark-theme')){
+        icon.src='image/icon/sun.png';
+        localStorage.setItem('theme','dark');
+    }else{
+        icon.src='image/icon/moon.png';
+        localStorage.setItem('theme','light');
+    }
+}
+
+function check(){
+    var konfirmasi = alert('Sorry, this feature is not available yet...');
+    if(konfirmasi == true){
+        
+    }else{
+    
+    }
+}
+
+// function onImage() {
+//     document.getElementById('logo').src = 'image/logo-chocolate.png'
+//     document.getElementById('logo').alt = 'chocolate'
+//   }
+  
+//   function outImage() {
+//     document.getElementById('logo').src = 'image/logo-white.png'
+//     document.getElementById('logo').alt = 'white'
+//   }
